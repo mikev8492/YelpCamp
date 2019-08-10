60da6860cd323b1d7efb571329c006d8cc5cd0dd
Michael Villarreal <mikev8492@gmail.com>
2019-08-09 23:10:00 -0500

    added styling

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const express = require("express"),
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 //RUN THIS TO REFRESH AND SEED DB WITH DATA:
 // seedDB();
 
--- seeds.js
+++ seeds.js
@@ -6,12 +6,12 @@ var data = [
   {
     name: "Cloud's rest",
     image: "https://img.hipcamp.com/images/c_limit,f_auto,h_1200,q_60,w_1920/v1433545941/hhou7vtldtiqvgjgsoxe/humboldt-redwoods-burlington-campground.jpg",
-    description: "This place got lots of freakin trees. And they big trees."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     name: "Granite Hill",
     image: "https://i2.wp.com/www.hachettebookgroup.com/wp-content/uploads/2019/01/WA_MoraCampground_NPS-publicdomain.jpg?resize=1080%2C1079&ssl=1",
-    description: "Huge granite hill with dope bathrooms and shit."
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
   {
     name: "Lake Lakey",
